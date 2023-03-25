0b6077afdfcf94630924b6a86d394f3861bba7d8
ryo29wx <59174187+ryo29wx@users.noreply.github.com>
2023-03-25 16:05:59 +0900

    _

--- typycal90/003.go
+++ typycal90/003.go
@@ -38,8 +38,8 @@ const (
 	Inf = 1 << 29
 )
 
+// これは単にBFSをしてるだけ
 func getdist(start int) {
-	//BFS
 	for i := 1; i <= N; i++ {
 		Dist[i] = Inf
 	}
@@ -63,17 +63,15 @@ func getdist(start int) {
 func main() {
 	sc.Split(bufio.ScanWords)
 	N = nextInt()
+	// 無向グラフ
 	for i := 1; i <= N-1; i++ {
 		a := nextInt()
 		b := nextInt()
-		fmt.Println("=======")
-		fmt.Println(a)
-		fmt.Println(b)
-		fmt.Println(G[a])
 		G[a] = append(G[a], b)
 		G[b] = append(G[b], a)
 	}
 
+	// 適当なところ(1番目)から一回BFSをする
 	getdist(1)
 	maxn1 := -1
 	maxid1 := -1
@@ -84,12 +82,14 @@ func main() {
 		}
 	}
 
+	// 最初のBFSで得られた、適当な位置から一番遠いノードから再度BFSをして一番遠いノードを見つける
 	getdist(maxid1)
 	maxn2 := -1.0
 	for i := 1; i <= N; i++ {
 		maxn2 = math.Max(float64(maxn2), float64(Dist[i]))
 	}
 
+	// 木の直径に対して+1((道を一本作って)してサイクルを作ればいいので +1を出力する
 	fmt.Println(maxn2 + 1)
 
 }
